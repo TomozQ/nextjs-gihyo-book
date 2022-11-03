349e52088bac674f0429d3b17c15678a2e2c20de
furuya <furuya.to@uhmfg.co.jp>
2022-11-03 22:32:55 +0900

    setting eslint and prettier

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,6 @@
-import { AppProps } from "next/app";
-import Head from "next/head"
-import { createGlobalStyle } from "styled-components";
+import { AppProps } from 'next/app'
+import Head from 'next/head'
+import { createGlobalStyle } from 'styled-components'
 
 // グローバルのスタイル
 const GlobalStyle = createGlobalStyle`
@@ -34,9 +34,13 @@ const MyApp = ({ Component, pageProps }: AppProps) => {
     <>
       <Head>
         <meta key="charset" name="charset" content="utf-8" />
-        <meta key="viewport" name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no, maximum-scales=5" />
-        <meta property="og:local" content="ja_JP"/>
-        <meta property="og:type" content="website"/>
+        <meta
+          key="viewport"
+          name="viewport"
+          content="width=device-width, initial-scale=1, shrink-to-fit=no, maximum-scales=5"
+        />
+        <meta property="og:local" content="ja_JP" />
+        <meta property="og:type" content="website" />
       </Head>
       <GlobalStyle />
       <Component {...pageProps} />

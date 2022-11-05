38d904556d6398b422d05fd3b2e460ccd2fa0f1e
furuya <furuya.to@uhmfg.co.jp>
2022-11-05 19:02:08 +0900

    install react-hook-form and sample components

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,7 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+import Form from './form'
 
 export default function Home() {
   return (
@@ -22,6 +23,12 @@ export default function Home() {
         </p>
 
         <div className={styles.grid}>
+
+          <a href="#" className={styles.card}>
+            <h2>React-Hook-Form &rarr;</h2>
+            <Form />
+          </a>
+
           <a href="https://nextjs.org/docs" className={styles.card}>
             <h2>Documentation &rarr;</h2>
             <p>Find in-depth information about Next.js features and API.</p>

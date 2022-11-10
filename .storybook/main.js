7f99c358d07568fa0748c5cbab255040a55118f2
furuya <furuya.to@uhmfg.co.jp>
2022-11-10 22:47:27 +0900

    storybook 設定修正

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,22 +1,18 @@
+
+
 const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin')
 const path = require('path')
 
 module.exports = {
-  "stories": [
-    "../src/**/*.stories.mdx",
-    "../src/**/*.stories.@(js|jsx|ts|tsx)"
+  stories: [
+    '../src/**/*.stories.mdx',
+    '../src/**/*.stories.@(js|jsx|ts|tsx)'
   ],
-  "addons": [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    // "@storybook/addon-interactions"
-    "@storybook/addon-postcss",
+  addons: [
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@storybook/addon-postcss',
   ],
-  "framework": "@storybook/react",
-  "core": {
-    "builder": "@storybook/builder-webpack5"
-  },
-  // 静的ファイルを配置するディレクトリを指定
   staticDirs: ['public'],
   babel: async options => ({
     ...options,
@@ -30,9 +26,9 @@ module.exports = {
     config.resolve.plugins = [
       new TsconfigPathsPlugin({
         configFile: path.resolve(__dirname, '../tsconfig.json')
-      })
+      }),
     ];
 
     return config
-  }
+  },
 }
--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,10 +1,10 @@
-import { addDecorator } from "@storybook/react"
-import { createGlobalStyle, ThemeProvider } from "styled-components"
+import { addDecorator } from '@storybook/react'
+import { createGlobalStyle, ThemeProvider } from 'styled-components'
 import { theme } from '../src/themes'
 import * as NextImage from 'next/image'
 
 export const parameters = {
-  actions: { argTypesRegex: "^on[A-Z].*" },
+  actions: { argTypesRegex: '^on[A-Z].*' },
   controls: {
     matchers: {
       color: /(background|color)$/i,
@@ -14,25 +14,22 @@ export const parameters = {
 }
 
 export const GlobalStyle = createGlobalStyle`
-html,
-body,
-textarea {
-  padding: 0;
-  margin: 0;
-  font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen,
-  Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif;
-}
-
-* {
-  box-sizing: border-box;
-}
-
-a {
-  cursor: pointer;
-  text-decoration: none;
-  transition: .25s;
-  color: #000;
-}
+  html,
+  body,
+  textarea {
+    padding: 0;
+    margin: 0;
+    font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen,
+      Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif;
+  }
+  * {
+    box-sizing: border-box;
+  }
+  a {
+    text-decoration: none;
+    transition: .25s;
+    color: #000000;
+  }
 `
 
 // Themeの適用
@@ -45,20 +42,6 @@ addDecorator((story) => (
 
 // next/imageの差し替え
 const OriginalNextImage = NextImage.default;
-
-Object.defineProperty(NextImage, 'default', {
-  configurable: true,
-  value: (props) => typeof props.src === 'string' ? (
-    <OriginalNextImage {...props} unoptimized blurDataURL={props.src} />
-    ):(
-    <OriginalNextImage {...props} unoptimized />
-  ),
-})
-
-// Object.defineProperty(NextImage, '__esModuleInterop', {
-//   configurable: true,
-//   value: true,
-// })
 
 Object.defineProperty(NextImage, 'default', {
   configurable: true,

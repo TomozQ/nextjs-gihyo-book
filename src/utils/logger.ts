3a07ae9ced8e9e656146e27c69614ab8affa6810
furuya <furuya.to@uhmfg.co.jp>
2022-11-30 22:14:40 +0900

    フォーマット

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -1,5 +1,5 @@
-import pino from "pino";
-import { createPinoBrowserSend, createWriteStream } from "pino-logflare";
+import pino from 'pino'
+import { createPinoBrowserSend, createWriteStream } from 'pino-logflare'
 
 const stream = createWriteStream({
   apiKey: `${process.env.NEXT_PUBLIC_LOGFLARE_API_KEY}`,
@@ -11,20 +11,20 @@ const send = createPinoBrowserSend({
   sourceToken: `${process.env.NEXT_PUBLIC_LOGFLARE_SOURCE_ID}`,
 })
 
-const logger = pino (
+const logger = pino(
   {
     browser: {
       transmit: {
         level: 'info',
         send: send,
-      }
+      },
     },
     level: 'debug',
     base: {
-      env: process.env.NODE_ENV
-    }
+      env: process.env.NODE_ENV,
+    },
   },
-  stream
+  stream,
 )
 
 export default logger
